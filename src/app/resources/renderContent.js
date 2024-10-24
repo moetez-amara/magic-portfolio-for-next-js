3ae0b4997fb765284fee8b306b91fb9491d71bdd
moetez-amara <moatez.amara@enicar.ucar.tn>
2024-10-24 15:33:26 +0100

    Revert "big fix 2"
    This reverts commit 3021c83f0441a4ad702ec57340484c51cff1dd41.

--- src/app/resources/renderContent.js
+++ src/app/resources/renderContent.js
@@ -1,5 +1,5 @@
 import { person, newsletter, social, home, about, blog, work, gallery } from './content';
-
+import { createI18nContent } from './content-i18n';
 import { i18n } from './config';
 
 const renderContent = (t) => {
