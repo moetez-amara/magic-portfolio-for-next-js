c1cd019a90ff6895058199a8fab6272b7db061d8
moetez-amara <moatez.amara@enicar.ucar.tn>
2024-10-24 15:33:11 +0100

    Revert "bugfix"
    This reverts commit f65579d8ca30e0231c9fed60880c45069b739f34.

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,4 +1,5 @@
 import { Flex, Heading } from '@/once-ui/components';
+import { Mailchimp } from '@/components';
 import { Posts } from '@/components/blog/Posts';
 import { baseURL, renderContent } from '@/app/resources'
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
